9662bef664f09a9156c4c8db861a7c4c3f303a23
HKShuttle <terakou1105@gmail.com>
2019-11-17 07:18:12 +0900

    maintained code

--- src/NekoMathLibs/test.java
+++ src/NekoMathLibs/test.java
@@ -2,19 +2,19 @@ package NekoMathLibs;
 
 public class test {
     public static void main(String[] args){
-        Equation.quadEqn(1.0, -5.0, 4.0);
+        EquationTest.quadEqn(1.0, -5.0, 4.0);
         System.out.println();
-        Equation.quadEqn(1.0, -2.0, 1.0);
+        EquationTest.quadEqn(1.0, -2.0, 1.0);
         System.out.println();
-        Equation.quadEqn(1.0, -5.0, 0);
+        EquationTest.quadEqn(1.0, -5.0, 0);
         System.out.println();
-        Equation.quadEqn(0, -5.0, 4.0);
+        EquationTest.quadEqn(0, -5.0, 4.0);
         System.out.println();
-        Equation.quadEqn(1.0, 0.0, 16.0);
+        EquationTest.quadEqn(1.0, 0.0, 16.0);
         System.out.println();
-        Equation.quadEqn(1.0, 3.0, 9.0);
+        EquationTest.quadEqn(1.0, 3.0, 9.0);
         System.out.println();
-        Equation.quadEqn(1.0, -6.0, 13.0);
+        EquationTest.quadEqn(1.0, -6.0, 13.0);
         System.out.println();
         quadSolver eq1 = new quadSolver(1.0, 0.0, 16.0);
         switch (eq1.ansType){
