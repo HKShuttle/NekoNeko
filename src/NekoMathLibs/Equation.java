b1f68f8c134d8b7522e5c006c5bd75e574b66036
HKShuttle <terakou1105@gmail.com>
2019-11-16 20:36:53 +0900

    It is successfully completed!

--- src/NekoMathLibs/Equation.java
+++ src/NekoMathLibs/Equation.java
@@ -1,11 +1,47 @@
 package NekoMathLibs;
 
 public class Equation {
-    public static double[] quadEqn(double a, double b, double c){
+    private static double discriminant;
+    public static void quadEqn(double a, double b, double c){
+
+        discriminant = b * b - 4 * a * c;
+        if(a == 0){
+            System.out.println("-------------------------");
+            System.out.println("a = 0 の場合は解を持たない");
+            System.out.println("-------------------------");
+            return;
+        }
+        if(discriminant > 0){
+            double[] result = isPositive(a, b, c);
+            System.out.println("-------------------------");
+            System.out.println("実数解: x = ( " + result[0] + ", " + result[1] + ")");
+            System.out.println("-------------------------");
+        } else if (discriminant == 0){
+            System.out.println("-------------------------");
+            System.out.println("重解: x = " + isZero(a, b, c));
+            System.out.println("-------------------------");
+        } else {
+            System.out.println("-------------------------");
+            System.out.println("虚数解: x = " + isNegative(a, b, c));
+            System.out.println("-------------------------");
+        }
+    }
+    private static double[] isPositive(double a, double b, double c){
         double[] result = new double[2];
-        double discriminant = b * b - 4 * a * c;
         result[0] = (- b +  Math.sqrt(discriminant)) / 2 * a;
         result[1] = (- b -  Math.sqrt(discriminant)) / 2 * a;
         return result;
+    }
+    private static double isZero(double a, double b, double c){
+        return (- b -  Math.sqrt(discriminant)) / 2 * a;
+    }
+    private static String isNegative(double a, double b, double c){
+        double ans1 = - ( b / (2 * a));
+        double ans2 = (Math.sqrt(- discriminant)) / (2 * a);
+        if((b / (2 * a)) == 0){
+            return "± " + ans2 + "i";
+        } else {
+            return ans1 + " ± " + ans2 + "i";
+        }
     }
 }
---
+++ src/NekoMathLibs/test.java
@@ -0,0 +1,16 @@
+package NekoMathLibs;
+
+public class test {
+    public static void main(String[] args){
+        Equation.quadEqn(1.0, -5.0, 4.0);
+        System.out.println();
+        Equation.quadEqn(1.0, -2.0, 1.0);
+        System.out.println();
+        Equation.quadEqn(1.0, -5.0, 0);
+        System.out.println();
+        Equation.quadEqn(0, -5.0, 4.0);
+        System.out.println();
+        Equation.quadEqn(1.0, 0.0, 16.0);
+        System.out.println();
+    }
+}
